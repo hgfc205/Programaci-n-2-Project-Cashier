a6a02e8d98256678fcce6eb6ba013d1e1d68ac8f
hgfc205 <hektorgermain@outlook.es>
2020-11-02 21:59:00 -0700

    Cajero!!

---
+++ Cajero.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <conio.h>
+#include <strings.h>
+
+char username[10], password[10];
+int intento, continuar;
+
+int main(){
+	intento=1;
+	std::cout<<" Bienvenido!!!\n";
+	
+	while (intento<4){
+		
+		std::cout<<"\n ingrese su nombre de usuario: "; 
+		std::cin>>username;
+		std::cout<<"\n ingrese su contraseña: ";
+		std::cin>>password;
+		
+		if (strcmp(username, "paco")== 0 && strcmp(password, "1234")== 0){
+			continuar=1;
+			while (continuar==1){
+			 std::cout<<"\n ¡¡Welcome!!\n";
+			 
+			 
+			 std::cout<<"\n Jejeje, thank you \n";
+			 std::cout<<"\n ¿Desea continuar? (1- continuar y 2- salir) ";
+			 std::cin>>continuar;
+			 
+			 while (continuar>2){
+			 	 std::cout<<"\n Ingrese el dato correcto (1- continuar y 2- salir) ";
+			 	 std::cin>>continuar;
+			 }
+			 
+			 if (continuar==2){
+			 	 intento=4;
+			 }
+			}
+		}
+		else{
+			intento++;
+			std::cout<<"\n  El nombre de usuario o la contraseña ingresada es erronea \n";
+			std::cout<<" Vuelva a ingresar sus datos \n";
+			
+			if (intento==4){
+                std::cout<<"-------------------------------------------------------------------------------------------";
+				std::cout<<"\n Ha ingresado la contra erroneamente 3 veces seguidas!!! \n";
+				std::cout<<" HA BLOQUEADO LA CUENTA!!!, F en el chat \n";
+			}
+		}
+	}
+	
+	std::cout<<"\n Come Back Anytime!!! :D \n";
+	getch();
+	return 0;
+}
