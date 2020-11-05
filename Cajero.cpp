1232864a98257d5a70dc35e43418ace5ec3aa079
hgfc205 <hektorgermain@outlook.es>
2020-11-05 13:14:18 -0700

    Cajero!!

--- Cajero.cpp
+++ Cajero.cpp
@@ -10,7 +10,7 @@ int main(){
 	std::cout<<" Bienvenido!!!\n";
 	
 	while (intento<4){
-		
+		std::cout<<"--------------------------------------------------------------------------";
 		std::cout<<"\n ingrese su nombre de usuario: "; 
 		std::cin>>username;
 		std::cout<<"\n ingrese su contraseña: ";
@@ -19,10 +19,12 @@ int main(){
 		if (strcmp(username, "paco")== 0 && strcmp(password, "1234")== 0){
 			continuar=1;
 			while (continuar==1){
-			 std::cout<<"\n ¡¡Welcome!!\n";
+		     system("cls");
+			 std::cout<<"--------------------------------------------------------------------------";
+			 std::cout<<"\n Welcome!!!\n";
 			 
 			 
-			 std::cout<<"\n Jejeje, thank you \n";
+			 std::cout<<"\n Jejeje, Thank You \n";
 			 std::cout<<"\n ¿Desea continuar? (1- continuar y 2- salir) ";
 			 std::cin>>continuar;
 			 
@@ -38,19 +40,22 @@ int main(){
 		}
 		else{
 			intento++;
+			system("cls");
 			if (intento<4){
+				std::cout<<"--------------------------------------------------------------------------";
 				std::cout<<"\n  El nombre de usuario o la contraseña ingresada es erronea \n";
 				std::cout<<" Vuelva a ingresar sus datos \n";
 			}
 			
 			if (intento==4){
-                std::cout<<"-------------------------------------------------------------------------------------------";
+                std::cout<<"--------------------------------------------------------------------------";
 				std::cout<<"\n Ha ingresado la contra erroneamente 3 veces seguidas!!! \n";
 				std::cout<<" HA BLOQUEADO LA CUENTA!!!, F en el chat \n";
 			}
 		}
 	}
-	
+	system("cls");
+	std::cout<<"--------------------------------------------------------------------------";
 	std::cout<<"\n Come Back Anytime!!! :D \n";
 	getch();
 	return 0;
